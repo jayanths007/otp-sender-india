d7281fb132989b0ecce72937755396ae8963d064
jayanths007 <jayanths007@gmail.com>
2021-09-14 09:07:06 +0200

    push to origin
    updated file pushed to origin first update

---
+++ react-otp/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import SendOtp from './components/SendOtp'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <SendOtp/>
+      </div>
+    );
+  }
+}
+
+export default App;
